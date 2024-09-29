7ddb3cef458837c78c68d97f34ecbceeb8248471
tonis2 <tanton@paysure.solutions>
2024-09-29 19:58:22 +0300

    Windows update

--- window.c3l/win32.c
+++ window.c3l/win32.c
@@ -15,24 +15,35 @@ typedef struct Window_Result {
 
 const char g_szClassName[] = "myWindowClass";
 
-
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-    switch(msg)
-    {
-        case WM_CLOSE:
-            DestroyWindow(hwnd);
-        break;
-        case WM_DESTROY:
-            PostQuitMessage(0);
-        break;
-        default:
-            return DefWindowProc(hwnd, msg, wParam, lParam);
-    }
-    return 0;
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+  if (message == WM_CLOSE && hwnd == window) return OnWindowClose(hwnd, message, wParam, lParam);
+  if (message == WM_SIZE && hwnd == window) return OnWindowResize(hwnd, message, wParam, lParam);
+  if (message == WM_PAINT && hwnd == window) return OnWindowPaint(hwnd, message, wParam, lParam);
+  return CallWindowProc(defWndProc, hwnd, message, wParam, lParam);
 }
 
-int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
+LRESULT OnWindowClose(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+  PostQuitMessage(0);
+  return CallWindowProc(defWndProc, hwnd, message, wParam, lParam);
+}
+
+LRESULT OnWindowResize(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+  LRESULT result = CallWindowProc(defWndProc, hwnd, message, wParam, lParam);
+  RECT rect;
+  GetClientRect(hwnd, &rect);
+  InvalidateRect(hwnd, &rect, false);
+  return result;
+}
+
+Window_Result APIENTRY createWindow(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
 {
-    return MessageBox(NULL, "hello, world", "caption", 0);
+    HWND window = CreateWindowEx(0, WC_DIALOG, L"Hello world (Paint)", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 316, 340, nullptr, nullptr, nullptr, nullptr);
+    WNDPROC defWndProc = nullptr;
+    defWndProc = (WNDPROC)SetWindowLongPtr(window, GWLP_WNDPROC, (LONG_PTR)WndProc);
+    ShowWindow(window, SW_SHOW);
+
+//     MSG message = { 0 };
+//   while (GetMessage(&message, nullptr, 0, 0))
+//     DispatchMessage(&message);
+//   return (int)message.wParam;
 }
