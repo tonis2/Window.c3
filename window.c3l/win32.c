03e73b8140ae3679b0422622d09b6c689a779246
tonis2 <tonis.anton18@gmail.com>
2024-09-30 09:46:50 -0700

    Working on windows api

--- window.c3l/win32.c
+++ window.c3l/win32.c
@@ -1,49 +1,192 @@
 #include <windows.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdio.h>
 
-typedef struct Size {
-    uint32_t width;
-    uint32_t height;
+typedef struct Size
+{
+  uint32_t width;
+  uint32_t height;
 } Size;
 
+typedef struct WindowSrc
+{
+  HWND window;
+  HINSTANCE instance;
+} WindowSrc;
+
 typedef struct Window_Result {
-    HWND window;
+    WindowSrc src;
     Size screen_info;
 } Window_Result;
 
+typedef struct Window_Params {
+    int depth;
+    int x;
+    int y;
+    int width;
+    int height;
+    int border_width;
+} Window_Params;
 
-const char g_szClassName[] = "myWindowClass";
-
-LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
-  if (message == WM_CLOSE && hwnd == window) return OnWindowClose(hwnd, message, wParam, lParam);
-  if (message == WM_SIZE && hwnd == window) return OnWindowResize(hwnd, message, wParam, lParam);
-  if (message == WM_PAINT && hwnd == window) return OnWindowPaint(hwnd, message, wParam, lParam);
-  return CallWindowProc(defWndProc, hwnd, message, wParam, lParam);
-}
-
-LRESULT OnWindowClose(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
-  PostQuitMessage(0);
-  return CallWindowProc(defWndProc, hwnd, message, wParam, lParam);
-}
-
-LRESULT OnWindowResize(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
-  LRESULT result = CallWindowProc(defWndProc, hwnd, message, wParam, lParam);
-  RECT rect;
-  GetClientRect(hwnd, &rect);
-  InvalidateRect(hwnd, &rect, false);
-  return result;
-}
-
-Window_Result APIENTRY createWindow(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
+LRESULT CALLBACK platform_window_callback(HWND window, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-    HWND window = CreateWindowEx(0, WC_DIALOG, L"Hello world (Paint)", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 316, 340, nullptr, nullptr, nullptr, nullptr);
-    WNDPROC defWndProc = nullptr;
-    defWndProc = (WNDPROC)SetWindowLongPtr(window, GWLP_WNDPROC, (LONG_PTR)WndProc);
+    switch (msg)
+    {
+
+    // case WM_DROPFILES:
+    // {
+    //     HDROP drop = (HDROP)wParam;
+
+    //     uint32_t fileCount = DragQueryFileA(drop, INVALID_IDX, 0, 0);
+
+    //     for (uint32_t i = 0; i < fileCount; i++)
+    //     {
+    //         uint32_t fileNameLength = DragQueryFileA(drop, i, 0, 0);
+
+    //         char filePath[500] = {};
+
+    //         DragQueryFileA(drop, i, filePath, fileNameLength + 1);
+
+    //         // Convert File to DDS and copy into Assets folder
+    //         {
+    //             char command[300] = {};
+
+    //             sprintf(command, "lib\\texconv.exe -y -m 1 -f R8G8B8A8_UNORM \"%s\" -o assets/textures",
+    //                     filePath);
+
+    //             CAKEZ_ASSERT(!system(command), "Failed to import Asset: %s", filePath);
+    //         }
+    //     }
+    // }
+    // break;
+
+    // case WM_KEYDOWN:
+    // case WM_KEYUP:
+    // {
+    //     bool isDown = msg == WM_KEYDOWN ? true : false;
+    //     uint32_t keyID = INVALID_IDX;
+
+    //     switch ((int)wParam)
+    //     {
+    //     case 'A':
+    //         keyID = KEY_A;
+    //         break;
+
+    //     case 'D':
+    //         keyID = KEY_D;
+    //         break;
+
+    //     case 'S':
+    //         keyID = KEY_S;
+    //         break;
+
+    //     case 'W':
+    //         keyID = KEY_W;
+    //         break;
+
+    //     case VK_ESCAPE:
+    //         keyID = KEY_ESC;
+    //         break;
+    //     }
+
+    //     if (keyID < KEY_COUNT)
+    //     {
+    //         // if (input.keys[keyID].isDown != isDown)
+    //         // {
+    //         input.keys[keyID].halfTransitionCount++;
+    //         input.keys[keyID].isDown = isDown;
+    //         // }
+    //     }
+    // }
+    // break;
+
+    // case WM_MOUSEMOVE:
+    //     input.mousePos = input.mousePos;
+    //     input.mousePos = {(float)GET_X_LPARAM(lParam),
+    //                       (float)GET_Y_LPARAM(lParam)};
+    //     break;
+
+    // case WM_LBUTTONDOWN:
+    // case WM_MBUTTONDOWN:
+    // case WM_RBUTTONDOWN:
+    // case WM_LBUTTONUP:
+    // case WM_MBUTTONUP:
+    // case WM_RBUTTONUP:
+    // {
+    //     bool isDown = msg == WM_LBUTTONDOWN || msg == WM_RBUTTONDOWN || msg == WM_MBUTTONDOWN
+    //                       ? true
+    //                       : false;
+
+    //     KeyID keyID = (msg == WM_LBUTTONDOWN || msg == WM_LBUTTONUP)
+    //                       ? KEY_LEFT_MOUSE
+    //                   : (msg == WM_MBUTTONDOWN || msg == WM_MBUTTONUP)
+    //                       ? KEY_MIDDLE_MOUSE
+    //                       : KEY_RIGHT_MOUSE;
+
+    //     input.keys[keyID].halfTransitionCount++;
+    //     input.keys[keyID].isDown = isDown;
+
+    //     input.mouseClickPos = {(float)GET_X_LPARAM(lParam),
+    //                            (float)GET_Y_LPARAM(lParam)};
+
+    //     break;
+    // }
+
+    case WM_CLOSE:
+        // running = false;
+        break;
+    }
+
+    return DefWindowProcA(window, msg, wParam, lParam);
+}
+
+Window_Result createWindow(Window_Params params)
+{
+  HWND window;
+  // HANDLE dc = GetDC(window);
+
+  HINSTANCE instance = GetModuleHandleA(0);
+
+  WNDCLASS wc;
+  wc.lpfnWndProc = platform_window_callback;
+  wc.hInstance = instance;
+  wc.lpszClassName = "vulkan_engine_class";
+
+  if (!RegisterClassA(&wc))
+  {
+      fprintf(stderr, "Could not register class.\n");
+      exit(EXIT_FAILURE);
+  }
+
+   uint32_t windowStyle = WS_THICKFRAME | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX | WS_MAXIMIZEBOX | WS_OVERLAPPED;
+
+    window = CreateWindowExA(
+    WS_EX_APPWINDOW,
+    "vulkan_engine_class",
+    "Pong",
+    windowStyle,
+    params.width, params.height, params.x, params.y, 0, 0, instance, 0);
+
+    if (window == 0)
+    {
+        fprintf(stderr, "Could not open display.\n");
+        exit(EXIT_FAILURE);
+    }
+
     ShowWindow(window, SW_SHOW);
 
-//     MSG message = { 0 };
-//   while (GetMessage(&message, nullptr, 0, 0))
-//     DispatchMessage(&message);
-//   return (int)message.wParam;
+
+    Window_Result result = {
+        {
+          window,
+          instance,
+        },
+        {
+          100,
+          100
+        },
+    };
+    return result;
+
 }
